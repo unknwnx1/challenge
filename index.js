c95c5b71d2030a7f9d9da29a5acc87c381b44ae0
unknwnx1 <andriatmanrian@gmail.com>
2022-04-01 18:50:24 +0700

    change promise

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const read = require('readline-sync')
 const fetch = require('node-fetch')
+const moment = require('moment');
+const chalk = require('chalk');
 
-const username = read.question('masukan username : ');
+//const username = read.question('masukan username : ');
 
-async function test() {
-    const hasil = await fetch(`https://api.github.com/users/${username}` , {
-        method : "GET"
+const getUsers = (username) => new Promise((resolve,reject) => {
+    fetch("https://api.github.com/users/" + username, {
+      method: "GET",
     })
-    .then(Response => Response.json())
-   const user = hasil.login
-   const followers = hasil.followers
-   const following = hasil.following
-   console.log(`username : ${user}`);
-   console.log(`Jumlah Followers : ${followers}`);
-   console.log(`Jumlah Following : ${following}`);
+      .then((res) => res.json())
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+});
+(async() => {
+    const username = read.question(chalk.green(`[${moment().format("HH:mm:ss")}] Masukan username : `))
+    const hasil = await getUsers(username);
+        if (!hasil.login) {
+            console.log(chalk.red(`[${moment().format("HH:mm:ss")}] Error` , hasil.message));
+        } else {
+            const user = hasil.login
+            const followers = hasil.followers
+            const following = hasil.following
+                console.log(chalk.green(`[${moment().format("HH:mm:ss")}] username : ${user}`));
+                console.log(chalk.green(`[${moment().format("HH:mm:ss")}] Jumlah Followers : ${followers}`));
+                console.log(chalk.green(`[${moment().format("HH:mm:ss")}] Jumlah Following : ${following}`));
 
-}
-test()
+
+        }
+})();
